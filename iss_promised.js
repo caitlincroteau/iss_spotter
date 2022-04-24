ef42a78e719cc9cb52cc67dd182e748b495047b0
Caitlin <cjcroteau@gmail.com>
2022-04-24 07:04:08 +0000

    add index2, iss_promised

---
+++ iss_promised.js
@@ -0,0 +1,31 @@
+// iss_promised.js
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request("https://api.ipify.org?format=json");
+};
+
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://api.freegeoip.app/json/${ip}?apikey=db8ecfe0-c29c-11ec-abaa-375097b78fd6`);
+
+}
+
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body);
+  const url = `https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+}
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then((data) => {
+    const { response } = JSON.parse(data);
+    return response;
+  });
+};
+
+//module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+module.exports = { nextISSTimesForMyLocation };
